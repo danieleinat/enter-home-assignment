a8d0c5b2c2b0f6dde672bedba0db3c39a9f38c95
Daniel Einat <191679047+danieleinat@users.noreply.github.com>
2025-08-06 18:14:42 +0300

    initial commit - home assignment files

---
+++ Enter_Daniel Einat/main.js
@@ -0,0 +1,5 @@
+function showMore() {
+  const paragraph = document.getElementById("short-description");
+  paragraph.innerText =
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, urna eu tincidunt consectetur, nisi nisl aliquet magna, nec pulvinar lorem quam sed dolor. Nulla facilisi. Curabitur eu justo a nulla dapibus iaculis. Aenean vel luctus est. Vivamus finibus lectus vel luctus tincidunt. Cras luctus, mi a sollicitudin volutpat, metus turpis vehicula orci, id luctus risus enim vitae justo. Proin posuere, mauris eget condimentum feugiat, nunc lorem suscipit sem, ut sollicitudin sapien nulla sed turpis.";
+}
